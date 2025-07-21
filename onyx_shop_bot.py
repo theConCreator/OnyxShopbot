cea76b534425604522486e4552b8458d5a8a6fb6
theConCreator <platinumpd631@gmail.com>
2025-07-21 16:30:18 +0300

    59

--- onyx_shop_bot.py
+++ onyx_shop_bot.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import asyncio
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
